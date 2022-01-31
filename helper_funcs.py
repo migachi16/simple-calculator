4ae21694e399540eb2a3214ae9a4080826708bcb
mgachi2 <mgachi2@illinois.edu>
2022-01-31 01:34:39 -0600

    Added buttons, initial calculation

--- helper_funcs.py
+++ helper_funcs.py
@@ -1,3 +1,17 @@
-
+import decimal
+import re
 
 def typed_out(x: str) -> int:
+    pass
+
+def execute(x: str) -> float:
+    nums = re.split('\*|\^|-|\+|\u00f7', x)
+    operators = list(filter(('').__ne__, re.split('[0-9, .]', x)))
+    total = 0
+
+    return 0.0
+
+#sta = '12.1*623^23\u00f70-33+1'
+#ops = list(filter(('').__ne__, re.split('[0-9, .]', sta)))
+#numz = re.split('\*|\^|-|\+|\u00f7', sta)
+#print(ops)
--- simple_calculator.py
+++ simple_calculator.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as psg
+import helper_funcs as hf
 
 psg.theme('dark green 7')
 
@@ -13,54 +14,102 @@ layout =  [
           [psg.B('1'), psg.B('2'), psg.B('3'), psg.B('+')],
           [psg.B('4'), psg.B('5'), psg.Button('6'), psg.B('-')],
           [psg.B('7'), psg.B('8'), psg.B('9'), psg.B('*')],
-          [psg.B('Clear'), psg.B('0'), psg.B('\u00f7'), psg.B('=', enable_events = True)],
 
-          [psg.B('Exit')],
+          [psg.B('.'), psg.B('0'), psg.B('^'), psg.B('\u00f7')],
 
-          [psg.T('Result:'), psg.Output(size = (20, 1), key = '-EQL-')]
+          [psg.B('('), psg.B('C'), psg.B('=', enable_events = True), psg.B(')')],
+
+          [psg.T('Result:'), psg.Output(size = (25, 1), key = '-EQL-'), psg.B('Exit')]
           
           ]
 
 window = psg.Window(title, layout, element_justification = 'c')
 
+expression = ''
+previous_ans = ''
+left_paren_ct, right_paren_ct = 0, 0
+
 while True:
-    expression = ''
     event, vals = window.read()
     match event:
+        #
         case psg.WIN_CLOSED:
             break
         case 'Exit':
             break
+        #
         case '1':
-            window['-EXP-'].update(1)
+            expression += '1'
+            window['-EXP-'].update(expression)
         case '2':
-            window['-EXP-'].update(2)    
+            expression += '2'  
+            window['-EXP-'].update(expression)
         case '3':
-            window['-EXP-'].update(3)
+            expression += '3'
+            window['-EXP-'].update(expression)
         case '4':
-            window['-EXP-'].update(4) 
+            expression += '4' 
+            window['-EXP-'].update(expression)
         case '5':
-            window['-EXP-'].update(5)
+            expression += '5'
+            window['-EXP-'].update(expression)
         case '6':
-            window['-EXP-'].update(6)    
+            expression += '6'
+            window['-EXP-'].update(expression)
         case '7':
-            window['-EXP-'].update(7)
+            expression += '7'
+            window['-EXP-'].update(expression)
         case '8':
-            window['-EXP-'].update(8) 
+            expression += '8' 
+            window['-EXP-'].update(expression)
         case '9':
-            window['-EXP-'].update(9)
+            expression += '9'
+            window['-EXP-'].update(expression)
         case '0':
-            window['-EXP-'].update(0)    
+            expression += '0'    
+            window['-EXP-'].update(expression)
         case '+':
-            window['-EXP-'].update('+')
+            expression += '+'
+            window['-EXP-'].update(expression)
         case '-':
-            window['-EXP-'].update('-') 
+            expression += '-'
+            window['-EXP-'].update(expression)
         case '\u00f7':
-            window['-EXP-'].update('\u00f7')
+            expression += '\u00f7'
+            window['-EXP-'].update(expression)
         case '*':
-            window['-EXP-'].update('*')    
+            expression += '*'    
+            window['-EXP-'].update(expression)
+        case '.':
+            expression += '.'    
+            window['-EXP-'].update(expression)
+        case '^':
+            expression += '^'    
+            window['-EXP-'].update(expression)
+        case '(':
+            expression += '('   
+            left_paren_ct += 1 
+            window['-EXP-'].update(expression)
+        case ')':
+            expression += ')'    
+            right_paren_ct += 1 
+            window['-EXP-'].update(expression)
+        #
+        #   Mundane buttons ^
+        #
         case '=':
-            print('Your expression is', expression)
-            window['-EXP-'].update()
+            if left_paren_ct != right_paren_ct:
+                print('Your parentheses do not match!')
+                continue
+            print('Your expression is '+ expression)
+            #
+            answer = hf.execute(expression) # Execute the algebraic expression
+            #
+            print('Your answer is ', answer)
+            window['-EXP-'].update(expression)
+        case 'C':
+            expression = ''
+            left_paren_ct, right_paren_ct = 0, 0
+            window['-EXP-'].update(expression)
 
 window.close()
