5057d55250314c2d1fae4d68b5596d033c94e70d
mgachi2 <migachi16@gmail.com>
2022-02-02 01:52:19 -0600

    very tired

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,18 +1,29 @@
 import PySimpleGUI as psg
 import helper_funcs as hf
 
+nums = '0123456789'
+
 psg.theme('dark green 7')
 psg.set_options(font = ("Fira Code", 16))
 
 title = 'Simple Calculator'
 
+# TODO 
+#   Implement
+menu_def =  [
+                ['File', ['Save log', 'Clear log', 'Reset', 'Quit',]],
+                ['Help', ['About',]]
+            ]
+
 layout =    [
 
                 [psg.T('Input an algebraic expression with the buttons or the keyboard.')], 
                 
+                [psg.Menu(menu_def)],
+
                 [psg.In(key = '-EXP-')],
 
-                [psg.B('1'), psg.B('2'), psg.B('3'), psg.B('+')],
+                [psg.B('1'), psg.B('2'), psg.B('3'), psg.B('+'), psg.B('ANS')],
                 [psg.B('4'), psg.B('5'), psg.Button('6'), psg.B('-')],
                 [psg.B('7'), psg.B('8'), psg.B('9'), psg.B('*')],
 
@@ -24,14 +35,11 @@ layout =    [
                 
             ]
 
-menu_def =  [
-
-            ]
-
 window = psg.Window(title, layout, return_keyboard_events = True, element_justification = 'c')
 
 expression = ''
 previous_ans = ''
+just_solved = False
 
 while True:
     event, vals = window.read()
@@ -40,6 +48,8 @@ while True:
         idx = event.index(':')
         event = event[:-(len(event) - idx)] # extended keyboard input handling for the form xxx:#
 
+    #if just_solved == True and event not in nums:
+        
     match event:
         #
         case psg.WIN_CLOSED:
@@ -96,11 +106,15 @@ while True:
                 #
                 answer = hf.execute(expression) # Execute the algebraic expression
                 #
+                just_solved = True
+                previous_ans = str(answer)
                 print(str(answer))
             else:
                 print('Your parentheses do not match!')
         case 'C': # clear
             expression = ''
+        case 'ANS': 
+            expression += previous_ans
 
     window['-EXP-'].update(expression)
 
